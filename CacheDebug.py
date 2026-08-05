6de8af59e2432274c7c48d029588cf56b33c4b67
Ben Gilroy <115704549+relic-stack@users.noreply.github.com>
2026-08-05 13:30:58 +0100

    directory cleanup

--- CacheDebug.py
+++
@@ -1,6 +0,0 @@
-import platform
-import sys
-
-print(sys.version)
-print(platform.machine())
-print(sys.executable)
--- ConnectionDebug.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-print("Starting...")
-r = requests.get("https://livetiming.formula1.com")
-print(r.status_code)
-print(requests.get("https://api.github.com").status_code)
